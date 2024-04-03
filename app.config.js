b544fcac039db8d483c8417be4480f7eabf13cbb
Sujith Varughese <sujithv15@gmail.com>
2024-04-02 23:15:21 -0400

    update constants for production

--- app.config.js
+++ app.config.js
@@ -1,5 +1,12 @@
+import * as dotenv from "dotenv"
+
 module.exports = {
   // use the variable if it's defined, otherwise use the fallback
-  EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || '',
+  API_URL: process.env.API_URL || '',
 
 };
+/*
+// eas doesn't load values from `.env`, so make sure they are loaded here
+dotenv.config({
+  path: [path.resolve(__dirname, '.env'), path.resolve(__dirname, '.env.local')],
+})*/
