1dc02eb449d6ea58a27313fd7dfb5e79b66b20ca
Sujith Varughese <sujithv15@gmail.com>
2024-04-02 20:23:06 -0400

    update tab bar with icons

--- navigation/TabNavigation.jsx
+++ navigation/TabNavigation.jsx
@@ -2,7 +2,8 @@ import {View, Text } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../screens/HomeScreen'
 import TreatmentScreen from '../screens/TreatmentScreen'
-
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
 const Tab = createBottomTabNavigator()
 const TabNavigation = () => {
 
@@ -14,13 +15,15 @@ const TabNavigation = () => {
         component={HomeScreen}
         options={{
           tabBarLabel: "Home",
+          tabBarIcon: () => <FontAwesome5 name="home" size={24} color="black" />
         }}
       />
       <Tab.Screen
         name="treatment"
         component={TreatmentScreen}
         options={{
-          tabBarLabel: "Treatment"
+          tabBarLabel: "Treatment",
+          tabBarIcon: () => <Fontisto name="doctor" size={24} color="black" />
         }}
       />
     </Tab.Navigator>
